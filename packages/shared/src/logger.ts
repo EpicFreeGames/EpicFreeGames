55deaa39dffacb9c0fd3a030566d726ec814d76c
Veeti K <veetik@protonmail.com>
2022-02-15 03:37:34 +0200

    logger to discord and console

---
+++ packages/shared/src/logger.ts
@@ -0,0 +1,20 @@
+import { WebhookClient, WebhookMessageOptions } from "discord.js";
+import { config } from "config";
+
+const hook = new WebhookClient({
+  url: config.loggingHookUrl,
+});
+
+export const logger = {
+  discord: async (options: WebhookMessageOptions) => {
+    return await hook.send(options);
+  },
+
+  console: (msg: string) => {
+    const date = new Date();
+    const time = `${date.toLocaleDateString()} - ${date.toLocaleTimeString()}`;
+    const debug = `@ ${time} - `;
+
+    console.log(debug + msg);
+  },
+};
