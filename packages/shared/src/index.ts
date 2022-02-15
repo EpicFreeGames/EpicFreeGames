156f488523bb897d6d7ef9420a3a1e1bb1fc5908
Veeti K <veetik@protonmail.com>
2022-02-15 03:44:03 +0200

    export everything

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,11 @@
+export * from "./data/types";
+export * from "./data/models";
+
+export * from "./interactions";
+export * from "./localisation";
+export * from "./localisation/languages";
+
+export * as db from "./data/dal";
+export * as embeds from "./embeds";
+
+export * from "./logger";
