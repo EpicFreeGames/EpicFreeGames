a4509960b881d42c5512d2566c4aab89f7cc5af5
Veeti K <veetik@protonmail.com>
2022-02-15 13:22:45 +0200

    debug command

---
+++ apps/interactions-endpoint/src/commands/debug.ts
@@ -0,0 +1,8 @@
+import { CommandTypes, embeds, SlashCommand } from "shared";
+
+export const command: SlashCommand = {
+  type: CommandTypes.EVERYONE,
+  needsGuild: true,
+  execute: async (i, guild, language) =>
+    i.reply({ embeds: [embeds.commands.debug(i.guildId!)], ephemeral: true }),
+};
--- apps/interactions-endpoint/src/commands/index.ts
+++ apps/interactions-endpoint/src/commands/index.ts
@@ -3,7 +3,11 @@ import { SlashCommand } from "shared";
 import { command as free } from "./free";
 import { command as up } from "./up";
 
+import { command as debug } from "./debug";
+
 export const commands: Map<string, SlashCommand> = new Map();
 
 commands.set("free", free);
 commands.set("up", up);
+
+commands.set("debug", debug);
