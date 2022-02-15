89bc131c5572a281f94916a4234faae5ec539c32
Veeti K <veetik@protonmail.com>
2022-02-15 03:39:32 +0200

    database models

---
+++ packages/shared/src/data/models.ts
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+import { ICommandLog, IGame, IGuild } from "./types";
+import { Languages } from "../localisation/languages";
+
+export const GameModel = mongoose.model<IGame>(
+  "game",
+  new mongoose.Schema<IGame>(
+    {
+      name: { type: String, required: true, unique: true },
+      imgUrl: { type: String, required: true },
+      start: { type: Number, required: true },
+      end: { type: Number, required: true },
+      sent: { type: Boolean, required: true },
+      confirmed: { type: Boolean, required: true },
+      price: { type: String, required: true },
+      store: {
+        name: { type: String, required: true },
+        url: { type: String, required: true },
+      },
+      slug: { type: String, required: true },
+    },
+    { timestamps: true }
+  )
+);
+
+export const GuildModel = mongoose.model<IGuild>(
+  "guild",
+  new mongoose.Schema<IGuild>(
+    {
+      guildId: { type: String, required: true },
+      roleId: { type: String, default: null },
+      channelId: { type: String, default: null },
+      language: { type: String, default: Languages.en },
+      webhook: {
+        type: {
+          id: String,
+          token: String,
+        },
+        default: null,
+      },
+    },
+    { timestamps: true }
+  )
+);
+
+export const CommandLogModel = mongoose.model<ICommandLog>(
+  "command-log",
+  new mongoose.Schema<ICommandLog>(
+    {
+      command: { type: String },
+      sender: {
+        id: { type: String },
+        tag: { type: String },
+      },
+      guildId: { type: String, default: null },
+      respondedIn: { type: Number },
+    },
+    { timestamps: true }
+  )
+);
