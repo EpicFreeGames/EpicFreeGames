2794341a09b8adc45a798afc779899a1f0bef874
Veeti K <veetik@protonmail.com>
2022-02-15 03:43:32 +0200

    logging embeds

---
+++ packages/shared/src/embeds/logs.ts
@@ -0,0 +1,97 @@
+import { MessageEmbed } from "discord.js";
+import { IGuild, ICommandLog } from "../data/types";
+import { CommandInteraction } from "../interactions";
+
+const addDbInfo = (guild: IGuild, embed: MessageEmbed) => {
+  embed.description +=
+    "**__DB Info__**" +
+    "\n" +
+    `Language: ${guild.language}` +
+    "\n" +
+    `Role ID: ${guild.roleId}` +
+    +"\n" +
+    `Channel ID: ${guild.channelId}` +
+    "\n" +
+    `Webhook: ${guild.webhook ? "✅" : "❌"}`;
+};
+
+export const command = (guild: IGuild | null, log: ICommandLog, dms: boolean) => {
+  const embed = new MessageEmbed({
+    title: `${log.command} ${dms ? "in DMs" : ""}`,
+    color: "#2f3136",
+    description:
+      "**__Command Info__**" +
+      "\n" +
+      `Sender: ${log.sender.tag}` +
+      "\n" +
+      `Sender ID: ${log.sender.id}` +
+      "\n" +
+      `Guild ID: ${log.guildId}` +
+      "\n" +
+      `Responded in: ${log.respondedIn} ms`,
+  }).setTimestamp();
+
+  if (guild) addDbInfo(guild, embed);
+
+  return embed;
+};
+
+export const channelSet = (guild: IGuild | null, i: CommandInteraction, channelId: string) => {
+  const embed = new MessageEmbed({
+    title: "Channel set",
+    description:
+      "**__Info__**" +
+      "\n" +
+      `User: ${i.user.tag}` +
+      "\n" +
+      `User ID: ${i.user.id}` +
+      "\n" +
+      `Guild ID: ${i.guildId}` +
+      "\n" +
+      `Set channel ID: ${channelId}`,
+  }).setTimestamp();
+
+  if (guild) addDbInfo(guild, embed);
+
+  return embed;
+};
+
+export const roleSet = (guild: IGuild | null, i: CommandInteraction, roleId: string) => {
+  const embed = new MessageEmbed({
+    title: "Role set",
+    description:
+      "**__Info__**" +
+      "\n" +
+      `User: ${i.user.tag}` +
+      "\n" +
+      `User ID: ${i.user.id}` +
+      "\n" +
+      `Guild ID: ${i.guildId}` +
+      "\n" +
+      `Set role ID: ${roleId}`,
+  }).setTimestamp();
+
+  if (guild) addDbInfo(guild, embed);
+
+  return embed;
+};
+
+export const languageSet = (guild: IGuild | null, i: CommandInteraction, language: string) => {
+  const embed = new MessageEmbed({
+    title: "Language set",
+    description:
+      "**__Info__**" +
+      "\n" +
+      `User: ${i.user.tag}` +
+      "\n" +
+      `User ID: ${i.user.id}` +
+      "\n" +
+      `Guild ID: ${i.guildId}` +
+      "\n" +
+      `Set language: ${language}`,
+  }).setTimestamp();
+
+  if (guild) addDbInfo(guild, embed);
+
+  return embed;
+};
