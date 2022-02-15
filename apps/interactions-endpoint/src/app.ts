abd36d778cab856b347c7feff2e1da89235921a6
Veeti K <veetik@protonmail.com>
2022-02-15 13:12:03 +0200

    connect endpoint to controller

--- apps/interactions-endpoint/src/app.ts
+++ apps/interactions-endpoint/src/app.ts
@@ -1,12 +1,13 @@
 import { config } from "config";
 import { verifyKeyMiddleware } from "discord-interactions";
 import express from "express";
+import { handleRequests } from "./handleRequest";
 
 export const createApp = () => {
   const app = express();
   app.set("trust proxy", "loopback");
 
-  app.post("/interactions", verifyKeyMiddleware(config.botPublicKey));
+  app.post("/interactions", verifyKeyMiddleware(config.botPublicKey), handleRequests);
 
   return app;
 };
