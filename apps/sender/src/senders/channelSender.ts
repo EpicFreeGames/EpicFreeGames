c1aef03f4895de08370d31f3c9799b2636ad68aa
Veeti K <veetik@protonmail.com>
2022-03-02 20:20:32 +0200

    add type

--- apps/sender/src/senders/channelSender.ts
+++ apps/sender/src/senders/channelSender.ts
@@ -42,6 +42,7 @@ export class ChannelSender {
           const log: ISendingLog = {
             guildId: guild.guildId,
             sendingId: this.sendingId,
+            type: "channel",
             result: {
               success: true,
               reason: null,
@@ -56,6 +57,7 @@ export class ChannelSender {
           const log: ISendingLog = {
             guildId: guild.guildId,
             sendingId: this.sendingId,
+            type: "channel",
             result: {
               success: false,
               reason: "",
