14581cc01a0ad3b7464350843220947fb70d7208
Veeti K <veetik@protonmail.com>
2022-05-18 03:16:23 +0300

    add tooltip

--- apps/web-ui/components/ColorSchemeToggle/ColorSchemeToggle.tsx
+++ apps/web-ui/components/ColorSchemeToggle/ColorSchemeToggle.tsx
@@ -1,4 +1,4 @@
-import { Button, useMantineColorScheme } from "@mantine/core";
+import { Button, Tooltip, useMantineColorScheme } from "@mantine/core";
 import { MoonStars, Sun } from "tabler-icons-react";
 import { buttonStyles } from "../Button";
 
@@ -6,15 +6,19 @@ export const ColorSchemeToggle = () => {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
   const { classes } = buttonStyles();
 
+  const dark = colorScheme === "dark";
+
   return (
-    <Button
-      onClick={() => toggleColorScheme()}
-      p={"0.5rem"}
-      variant="light"
-      color={"gray"}
-      className={classes.colorSchemeButton}
-    >
-      {colorScheme === "dark" ? <MoonStars size={20} /> : <Sun size={20} />}
-    </Button>
+    <Tooltip label={dark ? "Turn on light mode" : "Turn on dark mode"}>
+      <Button
+        onClick={() => toggleColorScheme()}
+        p={"0.5rem"}
+        variant="light"
+        color={"gray"}
+        className={classes.colorSchemeButton}
+      >
+        {dark ? <MoonStars size={20} /> : <Sun size={20} />}
+      </Button>
+    </Tooltip>
   );
 };
