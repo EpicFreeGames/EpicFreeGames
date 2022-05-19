979d665cddf7e29915e6d866fc0756901e176a62
Veeti K <veetik@protonmail.com>
2022-05-20 00:30:53 +0300

    remove commands page

--- apps/web-ui/pages/commands.tsx
+++
@@ -1,8 +0,0 @@
-import { NextPage } from "next";
-import { Layout } from "../components/Layout";
-
-const Commands: NextPage = () => <Layout title="Commands">Commands</Layout>;
-
-(Commands as any).auth = true;
-
-export default Commands;
