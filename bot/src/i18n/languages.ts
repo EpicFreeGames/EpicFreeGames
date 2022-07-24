de0b716ff6c399c6071af2f382c466343a90fe34
Veeti K <veeti@veetik.com>
2022-07-24 16:43:17 +0300

    convert language obj to map

--- bot/src/i18n/languages.ts
+++ bot/src/i18n/languages.ts
@@ -87,140 +87,35 @@ import uk_old from "./t10s/uk/old-messages.json" assert { type: "json" };
 import vi_commands from "./t10s/vi/commands.json" assert { type: "json" };
 import vi from "./t10s/vi/messages.json" assert { type: "json" };
 
-export const languages = {
-  af: {
-    code: "af",
-    englishName: "Afrikaans",
-    nativeName: "Afrikaans",
-  },
-  ar: {
-    code: "ar",
-    englishName: "Arabic",
-    nativeName: "عربي",
-  },
-  az: {
-    code: "az",
-    englishName: "Azerbaijani",
-    nativeName: "آذربایجان دیلی‎",
-  },
-  bg: {
-    code: "bg",
-    englishName: "Bulgarian",
-    nativeName: "български",
-  },
-  cs: {
-    code: "cs",
-    englishName: "Czech",
-    nativeName: "čeština",
-  },
-  de: {
-    code: "de",
-    englishName: "German",
-    nativeName: "Deutsch",
-  },
-  en: {
-    code: "en",
-    englishName: "English",
-    nativeName: "English",
-  },
-  es: {
-    code: "es",
-    englishName: "Spanish",
-    nativeName: "español",
-  },
-  fa: {
-    code: "fa",
-    englishName: "Persian",
-    nativeName: "فارسی",
-  },
-  fr: {
-    code: "fr",
-    englishName: "French",
-    nativeName: "français",
-  },
-  he: {
-    code: "he",
-    englishName: "Hebrew",
-    nativeName: "עברית",
-  },
-  hi: {
-    code: "hi",
-    englishName: "Hindi",
-    nativeName: "हिन्दी",
-  },
-  hu: {
-    code: "hu",
-    englishName: "Hungarian",
-    nativeName: "magyar",
-  },
-  id: {
-    code: "id",
-    englishName: "Indonesian",
-    nativeName: "Bahasa Indonesia",
-  },
-  it: {
-    code: "it",
-    englishName: "Italian",
-    nativeName: "italiano",
-  },
-  ja: {
-    code: "ja",
-    englishName: "Japanese",
-    nativeName: "日本語",
-  },
-  ka: {
-    code: "ka",
-    englishName: "Georgian",
-    nativeName: "ქართული",
-  },
-  ko: {
-    code: "ko",
-    englishName: "Korean",
-    nativeName: "한국어",
-  },
-  nl: {
-    code: "nl",
-    englishName: "Dutch",
-    nativeName: "Nederlands",
-  },
-  pl: {
-    code: "pl",
-    englishName: "Polish",
-    nativeName: "polski",
-  },
-  pt: {
-    code: "pt",
-    englishName: "Portuguese",
-    nativeName: "português",
-  },
-  ro: {
-    code: "ro",
-    englishName: "Romanian",
-    nativeName: "română",
-  },
-  ru: {
-    code: "ru",
-    englishName: "Russian",
-    nativeName: "русский",
-  },
-  tr: {
-    code: "tr",
-    englishName: "Turkish",
-    nativeName: "Türkçe",
-  },
-  uk: {
-    code: "uk",
-    englishName: "Ukrainian",
-    nativeName: "українська",
-  },
-  vi: {
-    code: "vi",
-    englishName: "Vietnamese",
-    nativeName: "Tiếng Việt",
-  },
-};
+export const languages = new Map<string, Language>([
+  ["af", { code: "af", englishName: "Afrikaans", nativeName: "Afrikaans" }],
+  ["ar", { code: "ar", englishName: "Arabic", nativeName: "عربي" }],
+  ["bg", { code: "bg", englishName: "Bulgarian", nativeName: "Български" }],
+  ["cs", { code: "cs", englishName: "Czech", nativeName: "Čeština" }],
+  ["de", { code: "de", englishName: "German", nativeName: "Deutsch" }],
+  ["en", { code: "en", englishName: "English", nativeName: "English" }],
+  ["es", { code: "es", englishName: "Spanish", nativeName: "Español" }],
+  ["fa", { code: "fa", englishName: "Persian", nativeName: "فارسی" }],
+  ["fr", { code: "fr", englishName: "French", nativeName: "Français" }],
+  ["he", { code: "he", englishName: "Hebrew", nativeName: "עברית" }],
+  ["hi", { code: "hi", englishName: "Hindi", nativeName: "हिन्दी" }],
+  ["hu", { code: "hu", englishName: "Hungarian", nativeName: "Magyar" }],
+  ["id", { code: "id", englishName: "Indonesian", nativeName: "Bahasa Indonesia" }],
+  ["it", { code: "it", englishName: "Italian", nativeName: "Italiano" }],
+  ["ja", { code: "ja", englishName: "Japanese", nativeName: "日本語" }],
+  ["ka", { code: "ka", englishName: "Georgian", nativeName: "ქართული" }],
+  ["ko", { code: "ko", englishName: "Korean", nativeName: "한국어" }],
+  ["nl", { code: "nl", englishName: "Dutch", nativeName: "Nederlands" }],
+  ["pl", { code: "pl", englishName: "Polish", nativeName: "Polski" }],
+  ["pt", { code: "pt", englishName: "Portuguese", nativeName: "Português" }],
+  ["ro", { code: "ro", englishName: "Romanian", nativeName: "Română" }],
+  ["ru", { code: "ru", englishName: "Russian", nativeName: "Русский" }],
+  ["tr", { code: "tr", englishName: "Turkish", nativeName: "Türkçe" }],
+  ["uk", { code: "uk", englishName: "Ukrainian", nativeName: "Українська" }],
+  ["vi", { code: "vi", englishName: "Vietnamese", nativeName: "Tiếng Việt" }],
+]);
 
-export const defaultLanguage: Language = languages["en"];
+export const defaultLanguage: Language = languages.get("en")!;
 export const defaultCurrency: Currency = {
   code: "USD",
   afterPrice: " $",
