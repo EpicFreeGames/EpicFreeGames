bfbd33c9517e6ad5b9f0c6771cc2ad6f7b1f47af
Veeti K <veeti@veetik.com>
2022-07-24 08:55:08 +0300

    apparently formatting, and send error msg instead of stack

--- bot/src/bot/events/interactionCreate.ts
+++ bot/src/bot/events/interactionCreate.ts
@@ -1,8 +1,4 @@
-import {
-  ApplicationCommandOptionTypes,
-  EventHandlers,
-  Interaction,
-} from "discordeno";
+import { ApplicationCommandOptionTypes, EventHandlers, Interaction } from "discordeno";
 import { logger } from "~logger";
 import { api } from "../../api.ts";
 import { defaultCurrency, defaultLanguage } from "../../i18n/languages.ts";
@@ -10,72 +6,69 @@ import { Server } from "../../types.ts";
 import { commands } from "../commands/mod.ts";
 import { bot } from "../mod.ts";
 
-export const interactionCreateHandler: EventHandlers["interactionCreate"] =
-  async (_bot, i) => {
-    const command = commands.get(i.data?.name ?? "");
-    if (!command) return;
+export const interactionCreateHandler: EventHandlers["interactionCreate"] = async (_bot, i) => {
+  const command = commands.get(i.data?.name ?? "");
+  if (!command) return;
 
-    if (command.needsGuild && !i.guildId) return;
+  if (command.needsGuild && !i.guildId) return;
 
-    let language = defaultLanguage;
-    let currency = defaultCurrency;
+  let language = defaultLanguage;
+  let currency = defaultCurrency;
 
-    const commandName = getCommandName(i);
+  const commandName = getCommandName(i);
 
-    const { error, data: server } = await api<Server | undefined>({
-      method: "GET",
-      path: `/servers/${i.guildId}`,
+  const { error, data: server } = await api<Server | undefined>({
+    method: "GET",
+    path: `/servers/${i.guildId}`,
+  });
+
+  if (!error && server) {
+    language = server.language;
+    currency = server.currency;
+  }
+
+  try {
+    logger.debug(`Executing command: ${command.name}`);
+
+    await command.execute({
+      bot,
+      i,
+      commandName,
+      server,
+      lang: language,
+      curr: currency,
     });
 
-    if (!error && server) {
-      language = server.language;
-      currency = server.currency;
-    }
+    const { error } = await api({
+      method: "POST",
+      path: "/logs/commands",
+      body: {
+        command: commandName,
+        senderId: String(i.user.id),
+        serverId: String(i.guildId),
+        error: null,
+      },
+    });
+    !!error && logger.error("Error logging command:", error);
 
-    try {
-      logger.debug(`Executing command: ${command.name}`);
+    logger.debug(`Command executed: ${command.name}`);
+    // deno-lint-ignore no-explicit-any
+  } catch (err: any) {
+    logger.error(`Command failed: ${command.name}, error: \n${err?.stack ?? err}`);
 
-      await command.execute({
-        bot,
-        i,
-        commandName,
-        server,
-        lang: language,
-        curr: currency,
-      });
-
-      const { error } = await api({
-        method: "POST",
-        path: "/logs/commands",
-        body: {
-          command: commandName,
-          senderId: String(i.user.id),
-          serverId: String(i.guildId),
-          error: null,
-        },
-      });
-      !!error && logger.error("Error logging command:", error);
-
-      logger.debug(`Command executed: ${command.name}`);
-      // deno-lint-ignore no-explicit-any
-    } catch (err: any) {
-      logger.error(
-        `Command failed: ${command.name}, error: \n${err?.stack ?? err}`
-      );
-
-      const { error } = await api({
-        method: "POST",
-        path: "/logs/commands",
-        body: {
-          command: commandName,
-          senderId: String(i.user.id),
-          serverId: String(i.guildId),
-          error: err?.stack ?? err,
-        },
-      });
-      !!error && logger.error("Error logging command:", error);
-    }
-  };
+    const { error } = await api({
+      method: "POST",
+      path: "/logs/commands",
+      body: {
+        command: commandName,
+        senderId: String(i.user.id),
+        serverId: String(i.guildId),
+        error: err?.message ?? "some error",
+      },
+    });
+    !!error && logger.error("Error logging command:", error);
+  }
+};
 
 const getCommandName = (i: Interaction): string => {
   if (!i.data) return "no-name";
