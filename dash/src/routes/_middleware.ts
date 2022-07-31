04d22f53a5026c8d0fc72a5d4480b4fd89f4886f
Veeti K <veeti@veetik.com>
2022-07-31 15:34:47 +0300

    dont check get the user in middleware

--- dash/src/routes/_middleware.ts
+++ dash/src/routes/_middleware.ts
@@ -1,9 +1,7 @@
 import { getCookies } from "$std/http/cookie.ts";
-import { IUser } from "../types.ts";
-import { api } from "../utils/api.ts";
 import { MiddlewareHandlerContext } from "../utils/freshTypes.ts";
 
-export const handler = async (req: Request, ctx: MiddlewareHandlerContext) => {
+export const handler = (req: Request, ctx: MiddlewareHandlerContext) => {
   const path = new URL(req.url).pathname;
 
   if (path === "/login" || path === "/discord-callback") return ctx.next();
@@ -18,24 +16,7 @@ export const handler = async (req: Request, ctx: MiddlewareHandlerContext) => {
       },
     });
 
-  const { error, data: user } = await api<IUser>({
-    method: "GET",
-    path: "/users/@me",
-    auth: cookies.sid,
-  });
-
-  error && console.error(error);
-
-  if (!user || error)
-    return new Response("", {
-      status: 303,
-      headers: {
-        location: "/login",
-      },
-    });
-
   ctx.state = {
-    user,
     auth: cookies.sid,
   };
 
--- dash/src/utils/freshTypes.ts
+++ dash/src/utils/freshTypes.ts
@@ -2,13 +2,8 @@ import {
   Handlers as FreshHandlers,
   MiddlewareHandlerContext as FreshMiddlewareHandlerContext,
 } from "$fresh/server.ts";
-import { User } from "../types.ts";
 
 export type State = {
-  /**
-   * Will be always defined, because it's checked in the middleware.
-   */
-  user: User;
   /**
    * Will be always defined, because it's checked in the middleware.
    */
