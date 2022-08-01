ee86972ef01a231f980e495ae09a110f6192ef46
Veeti K <veeti@veetik.com>
2022-08-01 03:44:57 +0300

    currency crud

--- api/src/routes/currencies.ts
+++ api/src/routes/currencies.ts
@@ -1,7 +1,9 @@
 import { prisma } from "..";
 import { auth } from "../utils/auth";
 import { Flags } from "../utils/flags";
+import { withValidation } from "../utils/withValidation";
 import { Router } from "express";
+import { z } from "zod";
 
 const router = Router();
 
@@ -10,5 +12,120 @@ router.get("/", auth(Flags.GetCurrencies), async (req, res) => {
 
   res.json(currencies);
 });
+
+router.get(
+  "/:currencyId",
+  auth(Flags.GetCurrencies),
+  withValidation(
+    {
+      params: z.object({
+        currencyId: z.string(),
+      }),
+    },
+    async (req, res) => {
+      const id = req.params.currencyId;
+
+      const currency = await prisma.currency.findUnique({
+        where: { id },
+      });
+
+      if (!currency)
+        return res.status(404).json({
+          statusCode: 404,
+          error: "Not found",
+          message: "Currency not found",
+        });
+
+      res.json(currency);
+    }
+  )
+);
+
+router.post(
+  "/",
+  auth(Flags.AddCurrencies),
+  withValidation(
+    {
+      body: z.object({
+        code: z.string(),
+        name: z.string(),
+        apiValue: z.string(),
+        inFrontOfPrice: z.string(),
+        afterPrice: z.string(),
+      }),
+    },
+    async (req, res) => {
+      const createdCurrency = await prisma.currency.create({
+        data: req.body,
+      });
+
+      res.json(createdCurrency);
+    }
+  )
+);
+
+router.patch(
+  "/:currencyId",
+  auth(Flags.GetCurrencies),
+  withValidation(
+    {
+      params: z.object({
+        currencyId: z.string(),
+      }),
+      body: z.object({
+        code: z.string().optional(),
+        name: z.string().optional(),
+        apiValue: z.string().optional(),
+        inFrontOfPrice: z.string().optional(),
+        afterPrice: z.string().optional(),
+      }),
+    },
+    async (req, res) => {
+      const id = req.params.currencyId;
+
+      const updatedCurrency = await prisma.currency.update({
+        where: { id },
+        data: req.body,
+      });
+
+      if (!updatedCurrency)
+        return res.status(404).json({
+          statusCode: 404,
+          error: "Not found",
+          message: "Currency not found",
+        });
+
+      res.json(updatedCurrency);
+    }
+  )
+);
+
+router.delete(
+  "/:currencyId",
+  auth(Flags.GetCurrencies),
+  withValidation(
+    {
+      params: z.object({
+        currencyId: z.string(),
+      }),
+    },
+    async (req, res) => {
+      const id = req.params.currencyId;
+
+      const deletedCurrency = await prisma.currency.delete({
+        where: { id },
+      });
+
+      if (!deletedCurrency)
+        return res.status(404).json({
+          statusCode: 404,
+          error: "Not found",
+          message: "Currency not found",
+        });
+
+      res.json(deletedCurrency);
+    }
+  )
+);
 
 export const currencyRouter = router;
