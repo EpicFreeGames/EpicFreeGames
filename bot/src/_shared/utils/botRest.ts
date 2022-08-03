59bcea2164cb283e57afb9ffc45c1ead789d3883
Veeti K <veeti@veetik.com>
2022-08-03 17:05:58 +0300

    fix import

--- bot/src/_shared/utils/botRest.ts
+++ bot/src/_shared/utils/botRest.ts
@@ -1,4 +1,4 @@
-import { createRestManager } from "https://deno.land/x/discordeno@13.0.0-rc45/mod.ts";
+import { createRestManager } from "discordeno";
 import { config } from "~config";
 
 export const botRest = createRestManager({
