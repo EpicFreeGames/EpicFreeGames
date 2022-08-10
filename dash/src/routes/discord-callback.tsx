b637b71598fd1913fe15608b6fb4573fc808a611
Veeti K <veeti@veetik.com>
2022-08-10 17:26:30 +0300

    debugging

--- dash/src/routes/discord-callback.tsx
+++ dash/src/routes/discord-callback.tsx
@@ -12,6 +12,8 @@ export const handler: Handlers = {
       path: `/auth/discord?code=${code}`,
     });
 
+    console.log({ response, error });
+
     if (error) return error;
 
     return new Response(null, {
