d3f1ae78f2527ac775799720e136cd5b2cd674d1
Veeti K <veeti@veetik.com>
2022-08-10 15:48:28 +0300

    add query params to api

--- bot/src/_shared/api.ts
+++ bot/src/_shared/api.ts
@@ -18,15 +18,17 @@ type Args =
       method: Exclude<Method, "GET" | "HEAD">;
       path: string;
       body?: Record<string, string | number | boolean | null | undefined>;
+      query?: URLSearchParams;
     }
   | {
       method: "GET" | "HEAD";
       path: string;
       body?: never;
+      query?: URLSearchParams;
     };
 
-export async function api<TData>({ method, path, body }: Args): Promise<ApiResponse<TData>> {
-  return fetch(`${config.EFG_API_BASEURL}${path}`, {
+export async function api<TData>({ method, path, body, query }: Args): Promise<ApiResponse<TData>> {
+  return fetch(`${config.EFG_API_BASEURL}${path}${query ? `?${query.toString()}` : ""}`, {
     method,
     headers: {
       "Content-Type": "application/json",
