e56ecf7c22bf7d6935f1c51e296078de52c9ca9a
Veeti K <veeti@veetik.com>
2022-08-12 22:19:22 +0300

    no optimistic update on game deletion

--- next-dash/src/utils/api/games/deleteGame.ts
+++ next-dash/src/utils/api/games/deleteGame.ts
@@ -1,36 +1,16 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { IGame } from "~types";
+import { useMutation } from "@tanstack/react-query";
 import { ApiError, apiRequest } from "../api";
+import { GameContext } from "./_sharedTypes";
 
 type DeleteGameProps = {
   gameId: string;
 };
 
-const deleteGame = ({ gameId }: DeleteGameProps) => apiRequest<void>(`/games/${gameId}`, "DELETE");
+const deleteGameRequest = ({ gameId }: DeleteGameProps) =>
+  apiRequest<void>(`/games/${gameId}`, "DELETE");
 
 export const useDeleteGameMutation = () => {
-  const qc = useQueryClient();
-  const mutation = useMutation<void, ApiError, DeleteGameProps, { prevGames: IGame[] }>(
-    deleteGame,
-    {
-      onMutate: async ({ gameId }) => {
-        await qc.cancelQueries(["games"]);
-
-        const prevGames = qc.getQueryData<IGame[]>(["games"]) ?? [];
-
-        qc.setQueryData(
-          ["games"],
-          prevGames.filter((g) => g.id !== gameId)
-        );
-
-        return { prevGames };
-      },
-      onError: (error, props, context) => {
-        context && qc.setQueryData(["games"], context.prevGames);
-      },
-      onSettled: () => qc.invalidateQueries(["games"]),
-    }
-  );
+  const mutation = useMutation<void, ApiError, DeleteGameProps, GameContext>(deleteGameRequest);
 
   return mutation;
 };
