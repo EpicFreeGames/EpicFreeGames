2c0d1969cd30eca6aff63fc598005f2f8107f33a
Veeti K <veeti@veetik.com>
2022-08-14 16:57:40 +0300

    white text

--- next-dash/src/components/StatusCard.tsx
+++ next-dash/src/components/StatusCard.tsx
@@ -4,11 +4,11 @@ type Props = { children: ReactNode; error?: boolean };
 
 export const StatusCard = ({ children, error }: Props) =>
   error ? (
-    <div className="rounded-md p-3 bg-red-800/95 border-[1px] border-red-600 flex items-center justify-center text-red-400">
+    <div className="rounded-md p-3 bg-red-800/95 border-[1px] border-red-600 flex items-center justify-center">
       {children}
     </div>
   ) : (
-    <div className="rounded-md p-3 bg-gray-800/95 border-[1px] border-gray-600 flex items-center justify-center text-gray-400">
+    <div className="rounded-md p-3 bg-gray-800/95 border-[1px] border-gray-600 flex items-center justify-center">
       {children}
     </div>
   );
