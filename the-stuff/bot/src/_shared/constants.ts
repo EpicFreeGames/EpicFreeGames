b134a1266e6b16a2827567668cd175f0b79bdd3b
Veeti K <veeti@veetik.com>
2022-08-24 16:22:18 +0300

    use front url in constants

--- the-stuff/bot/src/_shared/constants.ts
+++ the-stuff/bot/src/_shared/constants.ts
@@ -1,16 +1,18 @@
+import { sharedConfig } from "./sharedConfig.ts";
+
 export const botConstants = {
   inviteGif:
     "https://media1.tenor.com/images/8be041fe538a0f292bb85885768341a7/tenor.gif?itemid=5261112",
 
-  browserRedirect: (path: string) => `https://epicfreegames.net/r/browser${path}`,
-  launcherRedirect: (path: string) => `https://epicfreegames.net/r/launcher${path}`,
+  browserRedirect: (path: string) => `${sharedConfig.EFG_FRONT_BASEURL}/r/browser${path}`,
+  launcherRedirect: (path: string) => `${sharedConfig.EFG_FRONT_BASEURL}/r/launcher${path}`,
 
   website: {
-    home: "https://epicfreegames.net",
-    commands: "https://epicfreegames.net/commands",
-    tutorial: "https://epicfreegames.net/tutorial",
-    serverInvite: "https://discord.gg/49UQcJe",
-    botInvite: "https://epicfreegames.net/invite",
+    home: `${sharedConfig.EFG_FRONT_BASEURL}`,
+    commands: `${sharedConfig.EFG_FRONT_BASEURL}/commands`,
+    tutorial: `${sharedConfig.EFG_FRONT_BASEURL}/tutorial`,
+    serverInvite: `https://discord.gg/49UQcJe`,
+    botInvite: `${sharedConfig.EFG_FRONT_BASEURL}/invite`,
   },
 
   voteLinks: {
