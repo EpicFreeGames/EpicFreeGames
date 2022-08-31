b41d9a79e6b6e44af6e3c195eb5a8ea3cf63ce84
Veeti K <veeti@veetik.com>
2022-09-01 02:24:50 +0300

    unify env names

--- the-stuff/dash/src/utils/envs.ts
+++ the-stuff/dash/src/utils/envs.ts
@@ -1,5 +1,5 @@
-export const apiBaseUrl = process.env.NEXT_PUBLIC_EFG_API_BASE_URL!;
+export const apiBaseUrl = process.env.NEXT_PUBLIC_EFG_API_BASEURL!;
 export const wsUrl = process.env.NEXT_PUBLIC_EFG_API_WS_URL!;
 
-if (!apiBaseUrl) throw new Error("Missing environment variable NEXT_PUBLIC_EFG_API_BASE_URL");
+if (!apiBaseUrl) throw new Error("Missing environment variable NEXT_PUBLIC_EFG_API_BASEURL");
 if (!wsUrl) throw new Error("Missing environment variable NEXT_PUBLIC_EFG_API_WS_URL");
