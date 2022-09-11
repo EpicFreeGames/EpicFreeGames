6a8690e2b5ad0fde1b8e3a72daa721b3010e2bbe
Veeti K <veeti@veetik.com>
2022-09-12 01:36:18 +0300

    some error handling to slash command updater

--- apps/slash-commands/src/index.ts
+++ apps/slash-commands/src/index.ts
@@ -1,17 +1,28 @@
 import { configuration } from "@efg/configuration";
 import { logger } from "@efg/logger";
-import { discordApi } from "@efg/shared-utils";
+import { discordApi, objToStr } from "@efg/shared-utils";
 
 import { commands } from "./commands";
 
 (async () => {
   logger.info("Overwriting global slash commands...");
 
-  await discordApi({
-    method: "PUT",
-    path: `/applications/${configuration.DISCORD_BOT_ID}/commands`,
-    body: commands,
-  });
+  const { error } = await discordApi(
+    {
+      method: "PUT",
+      path: `/applications/${configuration.DISCORD_CLIENT_ID}/commands`,
+      body: commands,
+    },
+    { useProxy: false }
+  );
+
+  if (error) {
+    logger.error(
+      ["Failed to overwrite global slash commands", `Cause: ${objToStr(error)}`].join("\n")
+    );
+
+    process.exit(1);
+  }
 
   logger.info("Global slash commands updated");
 
