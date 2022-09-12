9c0d6d7dd3289670f072a55a577383b8d8ee6d28
Veeti K <veeti@veetik.com>
2022-09-13 02:31:54 +0300

    test command

--- apps/interactions-endpoint/src/commands/index.ts
+++ apps/interactions-endpoint/src/commands/index.ts
@@ -6,17 +6,19 @@ import { inviteCommand } from "./inviteCommand";
 import { removeCommand } from "./removeCommand";
 import { setCommand } from "./setCommand";
 import { settingsCommand } from "./settingsCommand";
+import { testCommand } from "./testCommand";
 import { upCommand } from "./upCommand";
 import { voteCommand } from "./voteCommand";
 
 export const commands: Map<string, SlashCommand> = new Map([
   ["set", setCommand],
   ["remove", removeCommand],
+  ["settings", settingsCommand],
+  ["test", testCommand],
   ["debug", debugCommand],
   ["free", freeCommand],
   ["help", helpCommand],
   ["invite", inviteCommand],
-  ["settings", settingsCommand],
   ["up", upCommand],
   ["vote", voteCommand],
 ]);
---
+++ apps/interactions-endpoint/src/commands/testCommand.ts
@@ -0,0 +1,31 @@
+import { MessageFlags, RESTPostAPIWebhookWithTokenJSONBody } from "discord-api-types/v10";
+
+import { embeds } from "@efg/embeds";
+import { discordApi, displayRole } from "@efg/shared-utils";
+
+import { interactionReply } from "../utils/interactions/responding/interactionReply";
+import { SlashCommand } from "../utils/interactions/types";
+
+export const testCommand: SlashCommand = {
+  needsGuild: true,
+  needsManageGuild: true,
+  execute: async ({ i, server, language, currency }, res) => {
+    if (!server?.channelId || !server.webhookId || !server.webhookToken)
+      return interactionReply(
+        { embeds: [embeds.errors.channelNotSet(language)], flags: MessageFlags.Ephemeral },
+        res
+      );
+
+    await discordApi(
+      {
+        method: "POST",
+        path: `/webhooks/${server.webhookId}/${server.webhookToken}`,
+        body: {
+          ...(server.roleId ? { content: displayRole(server.roleId) } : {}),
+          embeds: [{ title: "Testing", description: "Testing testing..." }],
+        } as RESTPostAPIWebhookWithTokenJSONBody,
+      },
+      { useProxy: false }
+    );
+  },
+};
