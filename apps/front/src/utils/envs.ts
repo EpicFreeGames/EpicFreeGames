e77a2bfe20a5dd35657929d6e987937f4e53df88
Veeti K <veeti@veetik.com>
2022-09-12 21:42:47 +0300

    remove front envs

--- apps/front/src/utils/envs.ts
+++ apps/front/src/utils/envs.ts
@@ -1,6 +1,3 @@
-export const token = process.env.VALID_FRONT_TOKEN;
-export const apiBaseUrl = process.env.EFG_API_BASEURL;
-
 export type IEnvironment = "Production" | "Staging";
 export const environment: IEnvironment = process.env.ENV as any;
 
