b332f819764269735898666bce04deb2cf3e4fef
Veeti K <veeti@veetik.com>
2022-09-26 14:40:11 +0300

    fix navbar padding when a dialog is open

--- apps/internal-dashboard/src/components/Layout/NavBar.tsx
+++ apps/internal-dashboard/src/components/Layout/NavBar.tsx
@@ -3,7 +3,7 @@ import { Menu } from "./Menu";
 
 export const NavBar = () => (
   <nav className="fixed z-10 w-full bg-gray-900/50 backdrop-blur-sm">
-    <div className="mx-auto flex h-[3.5rem] max-w-screen-lg items-center justify-between gap-3 px-3 ">
+    <div className="nav-bar mx-auto flex h-[3.5rem] max-w-screen-lg items-center justify-between gap-3 px-3">
       <Menu />
 
       <Logout />
