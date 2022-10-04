c6156516bea3048891ed119bbd310ba623be0a4e
Veeti K <veeti@veetik.com>
2022-10-05 00:41:34 +0300

    Update getActualISOString.ts

--- apps/internal-dashboard/src/utils/getActualISOString.ts
+++ apps/internal-dashboard/src/utils/getActualISOString.ts
@@ -3,5 +3,23 @@
  * @param date
  * @returns An ISO 8601 string (with timezone offset) for the given date
  */
-export const getActualISOString = (date: Date) =>
-  date.toISOString() + `${date.getTimezoneOffset() ? date.getTimezoneOffset() / 60 : ""}`;
+export const getActualISOString = (date: Date) => {
+  const isoString = date.toISOString();
+  const timezoneOffset = date.getTimezoneOffset();
+
+  if (!timezoneOffset) {
+    return isoString;
+  } else {
+    const timezoneOffsetHours = Math.abs(timezoneOffset / 60)
+      .toString()
+      .padStart(2, "0");
+    const timezoneOffsetMinutes = Math.abs(timezoneOffset % 60)
+      .toString()
+      .padStart(2, "0");
+
+    const timezoneOffsetString = `${timezoneOffsetHours}:${timezoneOffsetMinutes}`;
+    const timezoneOffsetSign = timezoneOffset > 0 ? "-" : "+";
+
+    return `${isoString.replace("Z", "")}${timezoneOffsetSign}${timezoneOffsetString}`;
+  }
+};
