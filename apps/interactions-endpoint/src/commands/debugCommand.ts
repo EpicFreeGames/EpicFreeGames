834f9408a6af915586973eed3333f61966eea9b7
Veeti K <veeti@veetik.com>
2022-12-20 16:52:55 +0200

    make debug command's response ephemeral

--- apps/interactions-endpoint/src/commands/debugCommand.ts
+++ apps/interactions-endpoint/src/commands/debugCommand.ts
@@ -1,3 +1,5 @@
+import { MessageFlags } from "discord-api-types/v10";
+
 import { embeds } from "@efg/embeds";
 
 import { interactionReply } from "../utils/interactions/responding/interactionReply";
@@ -7,5 +9,8 @@ export const debugCommand: SlashCommand = {
   needsGuild: true,
   needsManageGuild: false,
   execute: async ({ i, server, language, currency }, res) =>
-    interactionReply({ embeds: [embeds.commands.debug(i.guild_id)] }, res),
+    interactionReply(
+      { embeds: [embeds.commands.debug(i.guild_id)], flags: MessageFlags.Ephemeral },
+      res
+    ),
 };
