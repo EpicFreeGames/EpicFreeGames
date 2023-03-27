d3362b4465fa8172a7b32f56ebcf1ef963326a2e
Veeti K <veeti@veetik.com>
2023-03-27 22:54:33 +0300

    fix translate func

--- rust-api/discord/src/translate.rs
+++ rust-api/discord/src/translate.rs
@@ -11,17 +11,18 @@ impl Translator {
     pub fn new() -> Self {
         tracing::info!("Initializing translations...");
 
-        let mut translations = HashMap::new();
+        let mut languages_and_translations: HashMap<String, HashMap<String, String>> =
+            HashMap::new();
 
         let translations_dir = fs::read_dir("./discord/t10s").unwrap();
 
         for translation_dir in translations_dir {
             let translation_dir = translation_dir.unwrap();
-            let translation_dir_name = translation_dir.file_name().into_string().unwrap();
+            let language_code = translation_dir.file_name().into_string().unwrap();
 
             let translation_files = fs::read_dir(translation_dir.path()).unwrap();
 
-            let mut translation: HashMap<String, HashMap<String, String>> = HashMap::new();
+            let mut translations: HashMap<String, String> = HashMap::new();
 
             for translation_file in translation_files {
                 let translation_file = translation_file.unwrap();
@@ -29,19 +30,19 @@ impl Translator {
 
                 let translation_file_content = fs::read_to_string(translation_file.path()).unwrap();
 
-                translation.insert(
-                    translation_file_name.replace(".json", ""),
-                    serde_json::from_str(&translation_file_content).unwrap(),
+                translations.extend(
+                    serde_json::from_str::<HashMap<String, String>>(&translation_file_content)
+                        .unwrap(),
                 );
             }
 
-            translations.insert(translation_dir_name, translation);
+            languages_and_translations.insert(language_code, translations);
         }
 
         tracing::info!("Translations initialized");
 
         return Self {
-            translations: HashMap::new(),
+            translations: languages_and_translations,
         };
     }
 
