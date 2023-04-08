eb8164ea941a5b4b1d8dbd07f7a85b2992fd497b
Veeti K <veeti@veetik.com>
2023-04-08 23:58:58 +0300

    change log level

--- api/src/main.rs
+++ api/src/main.rs
@@ -18,7 +18,7 @@ pub mod types;
 #[tokio::main]
 async fn main() {
     tracing_subscriber::registry()
-        .with(filter::LevelFilter::DEBUG)
+        .with(filter::LevelFilter::INFO)
         .with(tracing_subscriber::fmt::layer())
         .init();
 
