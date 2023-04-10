d9cbd280d9165de1644866643398052cdc762c1b
Veeti K <veeti@veetik.com>
2023-04-10 15:42:21 +0300

    Update help_embed.rs

--- discord/src/embeds/help_embed.rs
+++ discord/src/embeds/help_embed.rs
@@ -13,48 +13,71 @@ pub fn help_embed(translator: &Translator, language: &Language) -> Embed {
         color: Some(embed_color(EmbedColor::Gray)),
         title: Some(translator.translate("help", language, None)),
         description: Some(format!(
-            "{help_desc}\n\n{looking_for_commands}\n\n{how_to_tutorial}\n\n{having_problems}\n\n{would_like_to_translate}\n{if_would_like_to_translate}",
+            "
+            {help_desc}
+            
+            {looking_for_commands}
+            
+            {how_to_tutorial}
+            
+            {having_problems}
+            
+            {would_like_to_translate}
+            {if_would_like_to_translate}
+            ",
             help_desc = format!("👋 {}", translator.translate("help_desc", language, None)),
-            looking_for_commands = format!("📋 {}", translator.translate(
-                "looking_for_commands",
-                language,
-                Some(&HashMap::from([(
-                    "commandsLink",
-                    CONSTANTS.website_commands_url.as_str()
-                )]))
-            )),
-            how_to_tutorial = format!("🎮 {}", translator.translate(
-                "how_to_tutorial",
-                language,
-                Some(&HashMap::from([(
-                    "tutorialLink",
-                    CONSTANTS.website_tutorial_url.as_str()
-                )]))
-            )),
-            having_problems = format!("🤔 {}", translator.translate(
-                "having_problems",
-                language,
-                Some(&HashMap::from([(
-                    "supportServerLink",
-                    CONSTANTS.server_invite_url.as_str()
-                )]))
-            )),
-            would_like_to_translate = format!("🚩 {}", bold(translator.translate(
-                "would_you_like_to_translate",
-                language,
-                Some(&HashMap::from([(
-                    "botName",
-                    CONSTANTS.bot_name.as_str()
-                )]))
-            ))),
-            if_would_like_to_translate =format!("- {}",  translator.translate(
-                "if_would_like_to_translate",
-                language,
-                Some(&HashMap::from([(
-                    "serverInvite",
-                    CONSTANTS.server_invite_url.as_str()
-                )]))
-            ))
+            looking_for_commands = format!(
+                "📋 {}",
+                translator.translate(
+                    "looking_for_commands",
+                    language,
+                    Some(&HashMap::from([(
+                        "commandsLink",
+                        CONSTANTS.website_commands_url.as_str()
+                    )]))
+                )
+            ),
+            how_to_tutorial = format!(
+                "🎮 {}",
+                translator.translate(
+                    "how_to_tutorial",
+                    language,
+                    Some(&HashMap::from([(
+                        "tutorialLink",
+                        CONSTANTS.website_tutorial_url.as_str()
+                    )]))
+                )
+            ),
+            having_problems = format!(
+                "🤔 {}",
+                translator.translate(
+                    "having_problems",
+                    language,
+                    Some(&HashMap::from([(
+                        "supportServerLink",
+                        CONSTANTS.server_invite_url.as_str()
+                    )]))
+                )
+            ),
+            would_like_to_translate = format!(
+                "🚩 {}",
+                bold(translator.translate(
+                    "would_you_like_to_translate",
+                    language,
+                    Some(&HashMap::from([("botName", CONSTANTS.bot_name.as_str())]))
+                ))
+            ),
+            if_would_like_to_translate = format!(
+                "- {}",
+                translator.translate(
+                    "if_would_like_to_translate",
+                    language,
+                    Some(&HashMap::from([(
+                        "serverInvite",
+                        CONSTANTS.server_invite_url.as_str()
+                    )]))
+                )
+            )
         )),
         author: None,
         fields: Vec::new(),
