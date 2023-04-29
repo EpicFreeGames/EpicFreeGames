5273b5a74ff19f762efa106975e454cf414232de
Veeti K <veeti@veetik.com>
2023-04-30 01:45:00 +0300

    add create response util

--- bun/src/utils.ts
+++ bun/src/utils.ts
@@ -1,3 +1,10 @@
 export function objToStr(obj: object) {
 	return JSON.stringify(obj, null, 2);
 }
+
+export function createResponse(status: number, body?: unknown) {
+	return new Response(!!body ? JSON.stringify(body) : undefined, {
+		status,
+		...(!!body && { headers: { "Content-Type": "application/json" } }),
+	});
+}
