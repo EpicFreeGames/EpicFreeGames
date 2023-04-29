f5298c21109603cbd513c3fce3c4f6c48367bf64
Veeti K <veeti@veetik.com>
2023-04-30 01:45:23 +0300

    Delete interactions.ts

--- bun/src/discord/interactions.ts
+++
@@ -1,8 +0,0 @@
-import { APIInteractionResponse } from "discord-api-types/v10";
-
-export const createResponse = (interactionResponse: APIInteractionResponse) => {
-	return new Response(JSON.stringify(interactionResponse), {
-		headers: { "Content-Type": "application/json" },
-		status: 200,
-	});
-};
