4004d1bfa9c2ca2b959d244bd6055c2435802cbd
Veeti K <veeti@veetik.com>
2023-04-30 01:45:10 +0300

    use new router

--- bun/src/index.ts
+++ bun/src/index.ts
@@ -1,11 +1,16 @@
 import { discordHandler } from "./discord/discordHandler";
+import { gamesRouter } from "./endpoints/games";
+import { rootHandler } from "./router/handler";
 
 Bun.serve({
 	port: 8000,
 	development: false,
 	async fetch(req) {
-		const response = await discordHandler(req);
+		const response = await rootHandler("/api", req, discordHandler, gamesRouter);
 
-		return response;
+		return (
+			response ||
+			new Response(JSON.stringify({ error: "Endpoint does not exist" }), { status: 404 })
+		);
 	},
 });
