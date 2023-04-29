5ae7487e6a0a317d634ac7403e006e978366409d
Veeti K <veeti@veetik.com>
2023-04-30 01:45:38 +0300

    create test games router

---
+++ bun/src/endpoints/games.ts
@@ -0,0 +1,15 @@
+import { get } from "../router/handler";
+import { router } from "../router/router";
+import { createResponse } from "../utils";
+
+export const gamesRouter = router(
+	"/games",
+	get("/", (req) => {
+		return createResponse(200, [
+			{
+				id: 1,
+				name: "Game 1",
+			},
+		]);
+	})
+);
