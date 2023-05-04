70a7f518188899a1b4dc57f9aae82c02effa577d
Veeti K <veeti@veetik.com>
2023-05-04 18:23:42 +0300

    game stuff not null

--- bun/src/db/schema.ts
+++ bun/src/db/schema.ts
@@ -1,12 +1,15 @@
+import { InferModel } from "drizzle-orm";
 import { boolean, datetime, mysqlTable, text, varchar } from "drizzle-orm/mysql-core";
 
 export const game = mysqlTable("games", {
 	id: varchar("id", { length: 36 }).primaryKey(),
-	name: varchar("name", { length: 255 }),
-	displayName: varchar("display_name", { length: 255 }),
-	imageUrl: text("image_url"),
-	startDate: datetime("start_date"),
-	endDate: datetime("end_date"),
-	confirmed: boolean("confirmed"),
-	path: varchar("path", { length: 255 }),
+	name: varchar("name", { length: 255 }).notNull(),
+	displayName: varchar("display_name", { length: 255 }).notNull(),
+	imageUrl: text("image_url").notNull(),
+	startDate: datetime("start_date").notNull(),
+	endDate: datetime("end_date").notNull(),
+	confirmed: boolean("confirmed").notNull(),
+	path: varchar("path", { length: 255 }).notNull(),
 });
+
+export type Game = InferModel<typeof game>;
