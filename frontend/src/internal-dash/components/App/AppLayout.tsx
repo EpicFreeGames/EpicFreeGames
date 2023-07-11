4cec26e66c2d3000b75849ffeabaf6e7ca0fbd18
Veeti K <veeti@veetik.com>
2023-07-11 18:44:39 +0300

    Update AppLayout.tsx

--- frontend/src/internal-dash/components/App/AppLayout.tsx
+++ frontend/src/internal-dash/components/App/AppLayout.tsx
@@ -1,13 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export function AppLayout() {
 	return (
 		<div className="flex flex-col fixed w-full gap-2 h-full">
 			<nav className="fixed z-10 w-full py-4 bg-gray-900/50 backdrop-blur-sm">
 				<div className="mx-auto flex h-full max-w-[1000px] items-center justify-between gap-3 px-3">
-					<span>menu</span>
-
-					<span>idk</span>
+					<div className="flex gap-2">
+						<Link to="games">games</Link>
+						<Link to="sends">sends</Link>
+					</div>
 				</div>
 			</nav>
 
